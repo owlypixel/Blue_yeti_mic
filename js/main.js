334900d6cd7b76498a02c8b45ce7800206013fa1
Owlypixel <owlypixel@gmail.com>
2017-07-09 03:18:10 +0300

    almost ready, need centering and optimization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-// $(function(){
-	
-// });
+(function(){
+	var mutebutton = document.querySelector('.mute-btn');
+	mutebutton.addEventListener('click', function(){
+		console.log(mutebutton);
+		mutebutton.classList.toggle('muted');
+	})
+
+	Draggable.create("#volume", {
+		type:"rotation",
+		throwProps:true
+	});
+
+
+})();
